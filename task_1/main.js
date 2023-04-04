fb00737eb18fdd761c76661b4c01c3d80a0e9bf5
GLeb Tarakanov <glebtgd@gmail.com>
2023-04-04 12:02:52 +0600

    Сделано задание 1

---
+++ task_1/main.js
@@ -0,0 +1,13 @@
+const string1 = document.getElementById('string-1')
+const string2 = document.getElementById('string-2')
+const string3 = document.getElementById('string-3')
+const string4 = document.getElementById('string-4')
+const string5 = document.getElementById('string-5')
+const string6 = document.getElementById('string-6')
+
+console.log(string3.innerText)
+console.log(string5.innerText)
+console.log(string2.innerText)
+console.log(string6.innerText)
+console.log(string4.innerText)
+console.log(string1.innerText)
