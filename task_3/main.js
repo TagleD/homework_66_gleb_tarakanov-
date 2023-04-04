2ac1ef33a068fab27c5eb7edd0027e2cbcb4edd0
GLeb Tarakanov <glebtgd@gmail.com>
2023-04-04 12:35:03 +0600

    Сделано задание 3

---
+++ task_3/main.js
@@ -0,0 +1,18 @@
+// const container = document.getElementById('container');
+//
+// for (let index = 1; index < 6; index++) {
+//     const element = document.createElement('div');
+//     element.innerText = `Element ${index}`;
+//     element.classList.add('element');
+//     container.append(element);
+// }
+
+
+const container = document.getElementById('container');
+
+for (let index = 1; index < 6; index++) {
+    const element = document.createElement('div');
+    element.innerText = `Element ${index}`;
+    element.classList.add('element');
+    container.append(element);
+}
